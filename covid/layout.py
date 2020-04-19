da708211337e089cbc9083117cbd28bac776a39b
Michele Vascellari <michele.vascellari@gmail.com>
2020-04-19 10:24:27 +0100

    Update layout

--- covid/layout.py
+++ covid/layout.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
 import plotly.express as px
+import plotly.graph_objects as go
 
 import datetime as dt
 
@@ -54,12 +55,76 @@ DEFAULT = "variazione_totale_positivi"
 
 
 def set_layout(app: Dash):
-    navbar = dbc.NavbarSimple(
-        brand="COVID-19 dashboard",
-        brand_href="#",
-        # color="primary",
+
+    dropdown = dbc.DropdownMenu(
+        children=[
+            dbc.DropdownMenuItem(
+                dbc.NavLink(
+                    "Dati",
+                    href="https://github.com/pcm-dpc/COVID-19",
+                    style={"color": "black"},
+                )
+            ),
+            dbc.DropdownMenuItem(
+                dbc.NavLink(
+                    "Mappa",
+                    href="https://github.com/openpolis/geojson-italy",
+                    style={"color": "black"},
+                )
+            ),
+            # dbc.DropdownMenuItem(divider=True),
+            # dbc.DropdownMenuItem("Entry 3"),
+        ],
+        nav=True,
+        in_navbar=True,
+        label="Links",
+    )
+    navbar = dbc.Navbar(
+        dbc.Container(
+            [
+                html.A(
+                    dbc.Row(
+                        [
+                            # dbc.Col(
+                            #     html.Img(
+                            #         src=app.get_asset_url("stayhome.jpeg"),
+                            #         height="80px",
+                            #     )
+                            # ),
+                            dbc.Col(
+                                dbc.NavbarBrand("COVID-19 Dashboard", className="ml-2")
+                            ),
+                        ],
+                        align="center",
+                        no_gutters=True,
+                    ),
+                    # href="https://plot.ly",
+                ),
+                dbc.NavbarToggler(id="navbar-toggler2"),
+                dbc.Collapse(
+                    dbc.Nav(
+                        [
+                            # dbc.NavItem(
+                            #     dbc.NavLink(
+                            #         "Dati", href="https://github.com/pcm-dpc/COVID-19"
+                            #     )
+                            # ),
+                            dropdown,
+                        ],
+                        className="ml-auto",
+                        navbar=True,
+                    ),
+                    id="navbar-collapse2",
+                    navbar=True,
+                ),
+            ],
+            fluid=True,
+        ),
+        className="mb-5 navbar-expand",
         color="dark",
         dark=True,
+        sticky="top",
+        expand=True,
     )
     app.layout = dbc.Container(
         [
@@ -117,7 +182,14 @@ def set_layout(app: Dash):
                                 justify="around",
                             ),
                             dbc.Row(
-                                dbc.Col(dcc.Loading(dcc.Graph(id="map-plot-italy"))),
+                                dbc.Col(
+                                    dcc.Loading(
+                                        dcc.Graph(
+                                            id="map-plot-italy",
+                                            # figure=go.Figure(data={}, layout={'height': 800})
+                                        )
+                                    )
+                                ),
                                 style={"marginTop": "10px"},
                             ),
                             dbc.Row(
