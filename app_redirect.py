104caf97bb74e592e5ee9384dc34bd7c0b344bc4
Michele Vascellari <michele.vascellari@gmail.com>
2020-04-25 10:47:57 +0100

    Add flask redirect app

---
+++ app_redirect.py
@@ -0,0 +1,14 @@
+from flask import Flask, redirect
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello():
+    return redirect("https://covid-dashboard-italy.herokuapp.com/", code=302)
+
+
+if __name__ == "__main__":
+    # Bind to PORT if defined, otherwise default to 5000.
+    # port = int(os.environ.get('PORT', 5000))
+    app.run(host="0.0.0.0", port=5000)
