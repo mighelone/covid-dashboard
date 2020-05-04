2244bef899b7f0bdf8cca10359230cdd5d879a90
Michele Vascellari <michele.vascellari@gmail.com>
2020-05-04 13:36:03 +0100

    Redefine province API

--- covid/api/resources/italy/provinces.py
+++ covid/api/resources/italy/provinces.py
@@ -11,6 +11,8 @@ class ItalyProvinceSchema(ma.SQLAlchemyAutoSchema):
         model = ItalyProvince
         load_instance = True
 
+    denominazione_regione = ma.auto_field("denominazione_regione", model=ItalyRegion)
+
 
 class RegionQueryArg(ma.SQLAlchemySchema):
     region = ma.auto_field("denominazione_regione", model=ItalyRegion, required=False)
@@ -30,15 +32,23 @@ def register_italy_provinces_api(bp: Blueprint):
             schema=ItalyProvinceSchema(many=True), description="List of Italian regions"
         )
         def get(self, kwargs):
-            """Get the list of Italian regions
+            """Get the list of Italian provinces
 
-            Return a list of Italian regions
+            Return a list of Italian provinces. The provinces can be filtered by region
             """
-            query = ItalyProvince.query
+            query = db.session.query(
+                ItalyProvince.codice_provincia,
+                ItalyProvince.codice_regione,
+                ItalyProvince.denominazione_provincia,
+                ItalyRegion.denominazione_regione,
+                ItalyProvince.lat,
+                ItalyProvince.long,
+            )
+            query = query.filter(ItalyProvince.codice_provincia < 200).filter(
+                ItalyProvince.codice_regione == ItalyRegion.codice_regione
+            )
             if kwargs:
                 query = query.filter(
-                    ItalyProvince.codice_regione == ItalyRegion.codice_regione
-                ).filter(
                     ItalyRegion.denominazione_regione == kwargs["denominazione_regione"]
                 )
             return query
